0992bc849a8e35c8e79d5e26cf5723104fb05cad
VoxWave <victor_bankowski@hotmail.com>
2018-12-10 17:22:27 +0200

    added some code to track which buttons are pressed

--- src/player.rs
+++ src/player.rs
@@ -7,9 +7,29 @@ use specs::{Entity, WriteStorage, System, WriteExpect};
 
 pub struct PlayerHandle(pub Entity);
 
-pub struct PlayerControlSystem;
+pub struct PlayerControlSystem {
+    button_states: HashMap<Direction, bool>,
+};
+
+impl PlayerControlSystem {
+    pub fn new() -> Self {
+        let button_states = HashMap::new();
+        button_states.insert(Direction::Up, false);
+        button_states.insert(Direction::Down, false);
+        button_states.insert(Direction::Left, false);
+        button_states.insert(Direction::Right, false);
+        PlayerControlSystem {
+            button_states,
+        } 
+    }
+}
+
+enum Direction {
+    Up, Down, Left, Right,
+}
 
 impl<'a> System<'a> for PlayerControlSystem {
+
     type SystemData = (
         WriteExpect<'a, PlayerHandle>,
         WriteStorage<'a, Velocity>,
