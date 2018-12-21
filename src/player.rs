7936f0195ef2170f2122d7a4fa19bfb904d7f6ee
Your Name <victor_bankowski@hotmail.com>
2018-12-21 22:07:36 +0200

    fixed the incorrect behaviour when pressing two buttons mapped to the same direction

--- src/player.rs
+++ src/player.rs
@@ -1,6 +1,6 @@
 use piston_window::keyboard::Key;
 use piston_window::{Button, Input};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::sync::mpsc::Receiver;
 
 use crate::na::{zero, Vector2};
@@ -20,17 +20,13 @@ pub struct PlayerHandle(pub Entity);
 
 pub struct PlayerControlSystem {
     button_map: HashMap<Button, Direction>,
-    button_states: HashMap<Direction, bool>,
+    button_states: HashSet<Button>,
     input_channel: Receiver<Input>,
 }
 
 impl PlayerControlSystem {
     pub fn new(input_channel: Receiver<Input>) -> Self {
-        let mut button_states = HashMap::new();
-        button_states.insert(Direction::Up, false);
-        button_states.insert(Direction::Down, false);
-        button_states.insert(Direction::Left, false);
-        button_states.insert(Direction::Right, false);
+        let mut button_states = HashSet::new();
         let mut button_map = HashMap::new();
         //TODO: make this configurable and not hardcoded.
         button_map.insert(Button::Keyboard(Key::W), Direction::Up);
@@ -50,24 +46,22 @@ impl PlayerControlSystem {
     }
 
     fn handle_inputs(&mut self) {
+        use piston_window::ButtonState::*;
         for input in self.input_channel.try_iter() {
             match input {
-                Input::Button(bs) => match self.button_map.get(&bs.button) {
-                    Some(dir) => {
-                        use piston_window::ButtonState::*;
-                        let pressed = self.button_states.get_mut(dir).unwrap();
-                        match bs.state {
-                            Press => {
-                                *pressed = true;
-                            }
-                            Release => {
-                                *pressed = false;
-                            }
+                Input::Button(bs) => {
+                    match bs.state {
+                        Press => {
+                            println!("press");
+                            self.button_states.insert(bs.button);
+                        },
+                        Release => {
+                            println!("release");
+                            self.button_states.remove(&bs.button);
                         }
                     }
-                    None => {}
                 },
-                _ => {}
+                _ => {},
             }
         }
     }
@@ -80,14 +74,13 @@ impl<'a> System<'a> for PlayerControlSystem {
         self.handle_inputs();
         let player_vel = velocities.get_mut(player.0).unwrap();
         let mut new_vel = zero::<Vector2<f32>>();
-        for (dir, pressed) in &self.button_states {
-            if *pressed {
-                new_vel += match dir {
-                    Direction::Up => Vector2::new(0., 1.),
-                    Direction::Down => Vector2::new(0., -1.),
-                    Direction::Left => Vector2::new(-1., 0.),
-                    Direction::Right => Vector2::new(1., 0.),
-                }
+        let pressed_buttons: HashSet<&Direction> = self.button_states.iter().flat_map(|b| self.button_map.get(b)).collect();
+        for dir in pressed_buttons {
+            new_vel += match dir {
+                Direction::Up => Vector2::new(0., 1.),
+                Direction::Down => Vector2::new(0., -1.),
+                Direction::Left => Vector2::new(-1., 0.),
+                Direction::Right => Vector2::new(1., 0.),
             }
         }
         if new_vel != zero() {
